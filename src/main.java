6e29937996cc792ab1fc2c2dbf6a07accaa7604e
Pawel <damian-pelka@wp.pl>
2020-05-23 09:49:36 +0100

    First commit change

--- src/main.java
+++ src/main.java
@@ -1,5 +1,6 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("Wprowadzenie mastera");
+        System.out.println("Pierwsza zmiana w masterze");
     }
 }
