e052667116008f66119649e0708354e3d912d1f3
Pawel <damian-pelka@wp.pl>
2020-05-23 09:51:01 +0100

    Second commit change

--- src/main.java
+++ src/main.java
@@ -2,5 +2,6 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Wprowadzenie mastera");
         System.out.println("Pierwsza zmiana w masterze");
+        System.out.println("Druga zmiana w masterze");
     }
 }
